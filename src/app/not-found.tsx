e0667698e5005bb7b40ef3315e6d68b32fa5fc28
Vivek-Gupta-15 <vivekgupta.151618@gmail.com>
2025-11-17 00:59:33 +0530

    Not-Found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound(){
+    return (
+        <div> 
+            <h2> Page Not Found.</h2>
+            <p> Tune link Galat click kiya hai. jakar pahele thik se check kr.</p>
+        </div>
+    )
+}
--- src/app/products/[productId]/reviews/[reviewsId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewsId]/page.tsx
@@ -1,8 +1,13 @@
+import { notFound } from "next/navigation";
+
 export default async function ProdectReview({
     params, 
 } : {
     params : Promise<{productId : string , reviewsId : string}>;
 }) {
     const {productId , reviewsId} = await params;
+    if(parseInt(reviewsId) > 100){
+        notFound();
+    }
     return <h1> Review {reviewsId} for Products {productId} is here.</h1>
 }
