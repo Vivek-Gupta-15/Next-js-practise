115d1d475c9350f791af451939883788b484d976
Vivek-Gupta-15 <vivekgupta.151618@gmail.com>
2025-11-17 03:28:55 +0530

    Dynamic Routing Metadata

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,8 +1,34 @@
-export default async function ProdectDetails({
-    params,
-} : {
-    params : Promise<{ productId : string}> ;
-}){
+// export default async function ProdectDetails({
+//     params,
+// } : {
+//     params : Promise<{ productId : string}> ;
+// }){
+//     const productId =(await params).productId;
+//     return  <h1> Details about Product {productId}.</h1>
+    
+// }
+
+//--Dynamic metadata routing
+
+import { Metadata } from "next";
+
+type Props = {
+    params : Promise<{productId : string}>;
+};
+
+export const generateMetadata = async ({params} : Props ) : Promise<Metadata>=> {
+    const id = (await params).productId;
+    const title = await new Promise((resolve) =>{
+        setTimeout (() => {
+            resolve(`Samsung ${id}`);
+
+        },100) ;
+    });
+    return {
+        title :  `Product${title}`,
+    };
+};
+export default async function ProdectDetails({ params } : Props){
     const productId =(await params).productId;
     return  <h1> Details about Product {productId}.</h1>
     
