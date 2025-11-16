5fce4ae382971396dbd68a78d9b3f669309ef4f4
Vivek-Gupta-15 <vivekgupta.151618@gmail.com>
2025-11-17 03:10:22 +0530

    Routing Metadata

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,7 @@
+export const metadata= {
+    title : "Shivam rani badi shayani",
+}
+
 export default function About(){
     return (
         <h1> About Me.</h1>
--- src/app/products/[productId]/reviews/[reviewsId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewsId]/page.tsx
@@ -6,8 +6,8 @@ export default async function ProdectReview({
     params : Promise<{productId : string , reviewsId : string}>;
 }) {
     const {productId , reviewsId} = await params;
-    if(parseInt(reviewsId) > 100){
-        notFound();
-    }
+    // if(parseInt(reviewsId) > 100){
+    //     notFound();
+    // }
     return <h1> Review {reviewsId} for Products {productId} is here.</h1>
 }
