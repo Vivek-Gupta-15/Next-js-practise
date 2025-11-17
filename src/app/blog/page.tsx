6df46b86116b8bfdb94620e6adac41a75d2fe963
Vivek-Gupta-15 <vivekgupta.151618@gmail.com>
2025-11-17 17:31:11 +0530

    Loading UI

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -7,8 +7,12 @@ export const metadata :  Metadata = {
     }
 }
 
-export default function Blog(){
-    return (
-        <h1> All Blogs will be shown here..</h1>
-    )
+export default async function Blog(){
+    await new Promise((resolve) => {
+        setTimeout(() => {
+        resolve("Wait till your page load..");
+
+        }, 2000); 
+    })
+    return  <h1> All Blogs will be shown here..</h1>
 }
