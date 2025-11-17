524f8e2882e0ecf319c60c8013c1485de8ffec44
Vivek-Gupta-15 <vivekgupta.151618@gmail.com>
2025-11-17 16:32:15 +0530

    Params and SearchParams using server side and client side.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,8 @@ export default function Home() {
     <h1> Welcome Home !! </h1>
     <Link href = "/blog "> Blog </Link>
     <Link href = " /products "> Product </Link>
+    <Link href = "/articles/breaking-news-Shivam-is-pregnent-by-water.?lang=en "> Read in English</Link>
+    <Link href = "/articles/breaking-news-Shivam-is-pregnent-by-water.?lang=fr "> Read in French</Link>
    </>
   );
 }
