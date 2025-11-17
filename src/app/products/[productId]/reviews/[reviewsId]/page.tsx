e587f001f278dae906707805366388a30a396fc0
Vivek-Gupta-15 <vivekgupta.151618@gmail.com>
2025-11-17 17:45:43 +0530

    Error Handling

--- src/app/products/[productId]/reviews/[reviewsId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewsId]/page.tsx
@@ -1,10 +1,18 @@
 import { notFound , redirect} from "next/navigation";
 
+function getRandomInt(count : number){
+    return Math.floor(Math.random() *count);
+}
+
 export default async function ProdectReview({
     params, 
 } : {
     params : Promise<{productId : string , reviewsId : string}>;
 }) {
+    const random = getRandomInt(2);
+    if(random === 1) {
+        throw new Error("Error loading review.")
+    }
     const {productId , reviewsId} = await params;
     if(parseInt(reviewsId) > 100){
         // notFound();
