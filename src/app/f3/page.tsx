42a797a9d2f609cb7dbfe35aea3e056993a35588
Vivek-Gupta-15 <vivekgupta.151618@gmail.com>
2025-11-20 17:36:11 +0530

    Intercepting Routes.

---
+++ src/app/f3/page.tsx
@@ -0,0 +1,3 @@
+export default function F3() {
+    return <h1> You have entered to F3 Page.</h1>
+}
---
+++ src/app/f5/page.tsx
@@ -0,0 +1,3 @@
+export default function F5() {
+    return <h1> You have entered to F5 page . </h1>
+}
